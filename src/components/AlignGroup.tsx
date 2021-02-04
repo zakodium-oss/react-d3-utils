bfd330318ca2dde85ca93842df33cdb6b1f26d02
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-02-04 17:05:38 +0100

    feat: align items in group

---
+++ src/components/AlignGroup.tsx
@@ -0,0 +1,67 @@
+import React, {
+  useLayoutEffect,
+  useRef,
+  useMemo,
+  useState,
+  ReactNode,
+} from 'react';
+
+type Align = 'start' | 'middle' | 'end';
+
+export interface AlignGroupProps {
+  x?: number;
+  y?: number;
+  verticalAlign?: Align;
+  horizontalAlign?: Align;
+  children: ReactNode | ReactNode[];
+}
+
+function calculatePosition(start: number, align: Align, space: number) {
+  switch (align) {
+    case 'start': {
+      return start;
+    }
+    case 'end': {
+      return start - space;
+    }
+    case 'middle': {
+      return start - space / 2;
+    }
+    default: {
+      throw new Error(`Unkwnown alignment ${JSON.stringify(align)}`);
+    }
+  }
+}
+
+export function AlignGroup(props: AlignGroupProps) {
+  const {
+    x = 0,
+    y = 0,
+    verticalAlign = 'start',
+    horizontalAlign = 'start',
+    children,
+  } = props;
+
+  const groupRef = useRef<SVGGElement | null>(null);
+  const [state, setState] = useState({ height: 0, width: 0, x: 0, y: 0 });
+  useLayoutEffect(() => {
+    const { height = 0, width = 0, x = 0, y = 0 } =
+      groupRef?.current?.getBBox() || {};
+    setState({ height, width, x, y });
+  }, [groupRef]);
+
+  const xPosition = useMemo(
+    () => calculatePosition(x - state.x, horizontalAlign, state.width),
+    [x, horizontalAlign, state.width, state.x],
+  );
+  const yPosition = useMemo(
+    () => calculatePosition(y - state.y, verticalAlign, state.height),
+    [y, verticalAlign, state.height, state.y],
+  );
+
+  return (
+    <g ref={groupRef} transform={`translate(${xPosition}, ${yPosition})`}>
+      {children}
+    </g>
+  );
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+export * from './components/AlignGroup';
 export * from './components/ResponsiveChart';
