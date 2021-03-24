2abaa5b4267670ae93bfe2c8226af67a92f24f93
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-03-24 11:18:08 +0100

    fix: measure the current string length in linear ticks

--- src/hooks/useLinearPrimaryTicks.ts
+++ src/hooks/useLinearPrimaryTicks.ts
@@ -1,5 +1,5 @@
 import type { ScaleContinuousNumeric } from 'd3-scale';
-import { MutableRefObject, useEffect, useState } from 'react';
+import { MutableRefObject, useEffect, useMemo, useState } from 'react';
 
 import { textDimentions } from '../utils';
 
@@ -17,14 +17,7 @@ interface Options {
 }
 
 const TEST_HEIGHT = '+1234567890';
-
-function formatTicks<Scale extends ScaleContinuousNumeric<number, number>>(
-  ticks: number[],
-  scale: Scale,
-  format: (d: number) => string,
-): PrimaryLinearTicks[] {
-  return ticks.map((val) => ({ label: format(val), position: scale(val) }));
-}
+const MAX_ITERATION = 20;
 
 export function useLinearPrimaryTicks<
   Scale extends ScaleContinuousNumeric<number, number>
@@ -34,57 +27,72 @@ export function useLinearPrimaryTicks<
   ref: MutableRefObject<SVGGElement | null>,
   options: Options = {},
 ): PrimaryLinearTicks[] {
-  const [fontSize, setFontSize] = useState(8);
+  const [ticks, setTicks] = useState<number[]>([]);
+
   const range = scale.range();
   if (!range) throw new Error('Range needs to be specified');
 
   const domain = scale.domain();
   if (!domain) throw new Error('Domain needs to be specified');
-  const {
-    scientificNotation = false,
-    minSpace = 4,
-    tickFormat = scientificNotation
-      ? (x) => x.toExponential(2)
-      : (x) => JSON.stringify(x),
-  } = options;
 
-  // Calculates the word density
+  const { scientificNotation = false, minSpace = 4 } = options;
+  const tickFormat: (d: number) => string = useMemo(
+    () =>
+      options.tickFormat || scientificNotation
+        ? (x) => x.toExponential(2)
+        : (x) => JSON.stringify(x),
+    [scientificNotation, options.tickFormat],
+  );
+
+  const axisLength = Math.abs(range[0] - range[1]);
+
+  // Calculates the tick number that fits in the given space
   useEffect(() => {
     if (ref.current) {
+      let tickNumber: number | undefined;
+      let repeat = true;
+      let ticks: number[] = [];
+
       if (direction === 'horizontal') {
-        const minFormated = tickFormat(domain[0]);
-        const maxFormated = tickFormat(domain[1]);
-        const maxLenWord =
-          minFormated.length > maxFormated.length ? minFormated : maxFormated;
-        const { width } = textDimentions(maxLenWord, ref);
-        setFontSize(Math.ceil(width / maxLenWord.length));
+        for (let count = 0; repeat && count < MAX_ITERATION; count++) {
+          // get next ticks
+          ticks = scale.ticks(tickNumber);
+          const formatedTicks = ticks.map(tickFormat);
+          tickNumber = Math.min(ticks.length, tickNumber || Infinity);
+
+          // get the current tick space
+          const { width } = textDimentions(formatedTicks.join(''), ref);
+          const size = width + (ticks.length - 1) * minSpace;
+
+          // repeats if the size is bigger than current space
+          repeat = size > axisLength && tickNumber > 1;
+          if (repeat) {
+            tickNumber = tickNumber - 1;
+          }
+        }
+
+        setTicks(ticks);
       } else {
         const { height } = textDimentions(TEST_HEIGHT, ref);
-        setFontSize(Math.ceil(height));
+        for (let count = 0; repeat && count < MAX_ITERATION; count++) {
+          // get next ticks
+          ticks = scale.ticks(tickNumber);
+          tickNumber = Math.min(ticks.length, tickNumber || Infinity);
+
+          // get the current tick space
+          const size = ticks.length * (height + minSpace) - minSpace;
+
+          // repeats if the size is bigger than current space
+          repeat = size > axisLength && tickNumber > 1;
+          if (repeat) {
+            tickNumber = tickNumber - 1;
+          }
+        }
+
+        setTicks(ticks);
       }
     }
-  }, [direction, domain, tickFormat, ref]);
+  }, [axisLength, direction, minSpace, ref, scale, tickFormat]);
 
-  const defaultTicks = scale.ticks();
-  const defaultLabels = defaultTicks.map(tickFormat);
-
-  const defaultLenght =
-    direction === 'horizontal'
-      ? defaultLabels.reduce(
-          (acc, curr) => acc + minSpace + fontSize * curr.length,
-          -1 * minSpace,
-        )
-      : defaultLabels.length * (fontSize + minSpace) - minSpace;
-
-  // Checks if the space is clear enought
-  const defaultNumTicks = defaultTicks.length;
-  const axisLength = Math.abs(range[0] - range[1]);
-  if (axisLength >= defaultLenght || defaultNumTicks < 2) {
-    return formatTicks(defaultTicks, scale, tickFormat);
-  }
-
-  // Gets number of ticks
-  const numTicks = Math.floor((defaultNumTicks * axisLength) / defaultLenght);
-  const calculatedTicks = scale.ticks(Math.max(2, numTicks - 1));
-  return formatTicks(calculatedTicks, scale, tickFormat);
+  return ticks.map((val) => ({ label: tickFormat(val), position: scale(val) }));
 }
