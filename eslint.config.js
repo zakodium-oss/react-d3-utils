82de2d93e0b9b0cd4524dac71391c8ea6907f4be
Michaël Zasso <targos@protonmail.com>
2025-08-14 10:44:38 +0200

    fix: exports (#47)

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import react from 'eslint-config-zakodium/react';
 import ts from 'eslint-config-zakodium/ts';
 import storybook from 'eslint-plugin-storybook';
 
-export default defineConfig(react, ts, storybook.configs['flat/recommended']);
+export default defineConfig(
+  globalIgnores(['lib', 'storybook-static']),
+  react,
+  ts,
+  storybook.configs['flat/recommended'],
+);
